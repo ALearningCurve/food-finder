d601c9569f7e5929da236db2a1c204b0cd23b5f0
Will WS <willwallsoto@gmail.com>
2022-08-02 17:15:17 -0400

    return result from bing raw

--- pages/api/map.ts
+++ pages/api/map.ts
@@ -14,10 +14,13 @@ export default async function handler(
     const url = `${process.env.BING_SEARCH_URL}?query=${q}&userLocation=${l}&key=${process.env.BING_KEY}`;
     try {
         const bingRes = await fetch(url);
-        if (bingRes == null) {
+        // if the fetch fails for any reason return error
+        if (bingRes == null || !bingRes.ok) {
             throw Error("Could not fetch map data");
         }
-        res.status(200).json(bingRes.json());
+        // get the response json and return it
+        const json = await bingRes.json();
+        res.status(200).json(json);
     } catch (error: any) {
         if (error instanceof Error) error = error.message;
         res.status(500).send({ error: String(error) });
