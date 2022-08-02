601b81ae46cce7c27b9f61e79b7d089a7ae3aab5
Will WS <willwallsoto@gmail.com>
2022-08-02 17:45:10 -0400

    working formatted response from next api converting bing api response

--- pages/api/map.ts
+++ pages/api/map.ts
@@ -1,13 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { ErrorMessage } from '../../interfaces/ErrorMessage';
 import { Locations, Location } from '../../interfaces/Location'
 
-type Data = {
-    name: string
-}
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<any>
+    res: NextApiResponse<Locations | ErrorMessage>
 ) {
     const q = "Dunkin";
     const l = "42.493160,-71.564568";
@@ -20,9 +18,26 @@ export default async function handler(
         }
         // get the response json and return it
         const json = await bingRes.json();
-        res.status(200).json(json);
+        // if the results are null, then just use an empty array to simulate the results
+        const results: any[] = json.resourceSets.flatMap((r: any) => r.resources) ?? [];
+        // TODO: Determine what resourceSets are and what resourceSet[n].estimatedTotal is
+        // convert our results array into the interface type that we defined for 
+        // results
+
+        const convertedResults: Location[] = results.map((res) => {
+            return {
+                name: res.name,
+                phone: res.PhoneNumber,
+                website: res.Website,
+                coordinates: res.geocodePoints.coordinates,
+                address: res.address
+            }
+        });
+        // map the returned JSON to our data type so that it can be displayed in the browser
+
+        res.status(200).json({ locations: convertedResults });
     } catch (error: any) {
         if (error instanceof Error) error = error.message;
-        res.status(500).send({ error: String(error) });
+        res.status(500).send({ errorMessage: String(error) });
     }
 }
