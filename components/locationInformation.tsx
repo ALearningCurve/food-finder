0e95a905192e0b9b034608fb731143e47e06132b
Will WS <willwallsoto@gmail.com>
2022-08-06 20:47:52 -0400

    display information in cards

--- components/locationInformation.tsx
+++ components/locationInformation.tsx
@@ -3,6 +3,17 @@ import { Location } from "../interfaces/Location"
 export default function LocationInformation(props: { location: Location }) {
     const { location } = props;
     return (
-        <p> location {location.name}</p>
+        <div className="border border-rounded mb-2 p-2 w-full" onClick={() => window.open(location.website, '_blank')}>
+            <h1>{location.name}</h1>
+            <div className="text-sm pl-2">
+                <p>
+                    {location.phone}
+                </p>
+                <p>
+                    23 Springwood Street, MA, USA
+                </p>
+                <a className="text-blue-600 underline" target="_blank" rel="noreferrer" href={location.website}>Web Site</a>
+            </div>
+        </div>
     )
 }
